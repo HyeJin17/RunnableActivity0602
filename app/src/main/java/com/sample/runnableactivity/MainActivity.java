8a1a742625c77390ba8174c0eae4b814354da121
HyeJin <rkd2313@gmail.com>
2021-06-02 15:19:12 +0900

    Runnable Inner Class

--- app/src/main/java/com/sample/runnableactivity/MainActivity.java
+++ app/src/main/java/com/sample/runnableactivity/MainActivity.java
@@ -1,14 +1,64 @@
-package com.sample.runnableactivity;
+package com.example.runnableactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    WorkerThread wt;
+    WorkerThread wr;
+
+    boolean running = true;
+    String TAG2 = "THREAD2";
+    String TAG = "THREAD";
+
+
+    class WorkerThread extends  Thread{
+        @Override
+        public void run() {
+            int i =0;
+            for(i = 0; i<20 && running; i++){
+                try{
+                    Thread.sleep(1000);
+
+                }catch (InterruptedException e){
+
+                }
+                Log.v(TAG,"Thread time = "+i);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        running = true;
+        wt= new WorkerThread();
+
+        wt.start();
+        wr.start();
+        Log.v(TAG2,"Now I am in onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        running = false;
+        Log.v(TAG2,"Now I am in onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG2,"Now I am in onPause");
+    }
+
+
 }
